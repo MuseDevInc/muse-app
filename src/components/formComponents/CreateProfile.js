c65bfdd415ce11356f0d2b4946d5abc0c64ba405
cdizon239 <coleencharmille@gmail.com>
2022-02-07 22:44:06 -0500

    update create profile form

--- src/components/formComponents/CreateProfile.js
+++ src/components/formComponents/CreateProfile.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Box, FormGroup, TextField, Typography, Button, Paper, Card } from "@mui/material";
+import {
+  Box,
+  FormGroup,
+  TextField,
+  Typography,
+  Button,
+  Paper,
+  Stack,
+  Autocomplete,
+} from "@mui/material";
 import { useTheme } from "@mui/material";
 import { CreateSubmitButton } from "./CreateSubmitButton";
 import { useNavigate } from "react-router-dom";
@@ -18,64 +27,75 @@ export function CreateProfile() {
 
   let backGrad = "linear-gradient(1deg, #00377C 40%, #F5F5F5)";
 
-
   return (
-    <Paper elevation={8} sx={{ minHeight: "100vh", maxHeight: "100vh", background: `${backGrad}` }}>
-
-    <Box>
-      <Typography
-        sx={{
-          textAlign: "center",
-          padding: "2rem",
-          margin: "1rem",
-          color: "white"
-        }}
-        variant="h1"
-      >
-        MUSE
-      </Typography>
-
-      <FormGroup sx={{ alignItems: "center", margin: "2rem" }}>
-        <TextField
-          id="outlined-basic"
-          label="Location"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          label="About Me"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          label="Favorite Genre"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          label="Top 1 Song"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          label="Top 2 Song"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          label="Top 3 Song"
-          variant="outlined"
-          margin="dense"
-        />
-        {/* <CreateSubmitButton /> */}
-        <CreateSubmitButton handleCreateSubmit={handleCreateSubmit} /> 
-      </FormGroup>
-    </Box>
+    <Paper
+      elevation={8}
+      sx={{ minHeight: "100vh", maxHeight: "100vh", background: `${backGrad}` }}
+    >
+      <Box>
+        <Typography
+          sx={{
+            textAlign: "center",
+            padding: "1%",
+            margin: "1%",
+            color: "white",
+          }}
+          variant="h1"
+        >
+          MUSE
+        </Typography>
+        <FormGroup sx={{ alignItems: "center", margin: "2rem" }}>
+          <TextField
+            id="location"
+            label="Location"
+            variant="outlined"
+            margin="dense"
+          />
+          <TextField
+            id="about-me"
+            label="About Me"
+            variant="outlined"
+            margin="dense"
+            multiline
+            rows={4}
+          />
+          <Autocomplete
+            multiple
+            id="tags-standard"
+            options={["Pop", "Rock", "Jazz"]}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="outlined"
+                label="Favorite Genres"
+                placeholder="Favorite Genres"
+              />
+            )}
+            margin="dense"
+          />
+          <TextField
+            id="favorite_song1"
+            label="Top 1 Song"
+            variant="outlined"
+            margin="dense"
+          />
+          <TextField
+            id="favorite_song2"
+            label="Top 2 Song"
+            variant="outlined"
+            margin="dense"
+          />
+          <TextField
+            id="favorite_song3"
+            label="Top 3 Song"
+            variant="outlined"
+            margin="dense"
+          />
+          <CreateSubmitButton />
+        </FormGroup>
+      </Box>
     </Paper>
   );
 }
