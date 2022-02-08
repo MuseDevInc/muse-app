b51e1a2bbd4877ef57a8239be7bf45ced092c018
cdizon239 <coleencharmille@gmail.com>
2022-02-08 00:59:22 -0500

    search top 1 song, ping + get data from spotifyweb

--- src/components/formComponents/CreateProfile.js
+++ src/components/formComponents/CreateProfile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SpotifyWebApi from "spotify-web-api-node";
 import useSpotifyAuth from '../../hooks/useSpotifyAuth';
 import {
@@ -19,25 +19,32 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 export function CreateProfile({ spotifyCode }) {
-
   const theme = useTheme();
   //handlers, will need state and setstate props. Can add popovers/helpers and additional validation/error handling feedback.
-
-  let navigate = useNavigate();
-
-  let handleCreateSubmit = (e) => {
-    e.preventDefault();
-    navigate("/userprofile");
-  };
-
   let backGrad = "linear-gradient(1deg, #00377C 40%, #F5F5F5)";
   const accessToken = useSpotifyAuth(spotifyCode);
-
-  //    run when access token refreshes
+  const [searchTopOne, setSearchTopOne] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  
+  // run when access token refreshes
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
+
+  // run everytime search params from users and spotify access token change
+  useEffect(() => {
+    if(!searchTopOne) return setSearchResults([])
+    if(!accessToken) return
+    //  use spotify web api to get seacrh results
+    spotifyApi.searchTracks(searchTopOne).then((res) => {
+      console.log(res.body);
+      // setSearchResults(
+        
+      // )
+    })
+  }, [setSearchResults, searchTopOne])
+
 
   return (
     <Paper
@@ -92,6 +99,7 @@ export function CreateProfile({ spotifyCode }) {
             label="Top 1 Song"
             variant="outlined"
             margin="dense"
+            onChange={(e) => setSearchTopOne(e.target.value)}
           />
           <TextField
             id="favorite_song2"
