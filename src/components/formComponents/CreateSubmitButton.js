658a5372b1c63897269c446cc4cac240ea404b84
cdizon239 <coleencharmille@gmail.com>
2022-02-10 22:48:23 -0500

    clean up and create profile button

--- src/components/formComponents/CreateSubmitButton.js
+++ src/components/formComponents/CreateSubmitButton.js
@@ -4,9 +4,7 @@ import { Button, Typography } from "@mui/material";
 export function CreateSubmitButton({ handleCreateSubmit}) {
   return (
     <Button onClick={handleCreateSubmit} variant="outlined">
-      Sign Up
-      {/* <Typography variant="h2" color="secondary"> */}
-      {/* </Typography> */}
+      Create Profile
     </Button>
   );
 }
