da4b507db62dde58a82816ddf28d862aa3195286
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-16 04:39:42 -0700

    alert, needs additional styling, links, advanceQ behavior

--- src/components/discover/DiscoverLayout.js
+++ src/components/discover/DiscoverLayout.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+
 import { DiscoverPaper } from "./DiscoverPaper";
 import { ThumbDownOffAltRounded, ThumbUp } from "@mui/icons-material";
-import { Box, Typography, Stack, IconButton, Paper } from "@mui/material";
-import { PlaybackControls } from "./PlaybackControls";
-import MatchActionButtons from "./MatchActionButtons";
-import NextAvatar from "./NextAvatar";
-import MusicPlayer from "../user/MusicPlayer";
+import { Box, Typography, IconButton, Paper } from "@mui/material";
 
+import MatchActionButtons from "./MatchActionButtons";
+
+import MatchSnackbar from "../feedbackAndNotifs/MatchSnackbar";
+import MuiAlert from '@mui/material/Alert'
 export function DiscoverLayout({ userQueue, qCounter }) {
-  //seeing if state update triggers render in a consistent way
-  const navigate = useNavigate()
   const [count, setCount] = useState();
   const [match, setMatch] = useState();
-  const titleRef = useRef(null)
 
   console.log(qCounter);
-  useEffect(()=>{
-    console.log(userQueue.length + "is user queue length and counter is at " + qCounter.current)
-  })
-
-  //handle user action
+  useEffect(() => {
+    console.log(
+      userQueue.length +
+        "is user queue length and counter is at " +
+        qCounter.current
+    );
+  });
 
   function handleSwipe(swipe) {
     pushId(swipe);
@@ -51,37 +50,26 @@ export function DiscoverLayout({ userQueue, qCounter }) {
     //this will be to check other user's swiperight array for currentUser's id.
     //if match, set match to true --> alert --> render FAB or change styling or open FAB
     const payload = {
-      swipedUser : userQueue[qCounter.current]._id
-    }
-    fetch(
-      process.env.REACT_APP_BACKEND_SERVER +
-        "/muse/discover/checkmatch/",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify(payload)
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          console.log(res);
-          if (res.status === 400) {
-            console.log("error");
-          }
-       if (res === true) {
-         console.log(res)
-         console.log("is response")
-         alert("match!")
-         console.log("match")
-         advanceQ()
-       }
-       if (res === false)
-       console.log("not a match")
-       advanceQ()
-        })
-    ;
-    //Need to determine how we are advancing queue on match, because we don't want to prevent user from interacting with match prematurely. User input should be required to advance queue after match notification.
-    //FAB buttons should receive all necessary props to render
+      swipedUser: userQueue[qCounter.current]._id,
+    };
+    fetch(process.env.REACT_APP_BACKEND_SERVER + "/muse/discover/checkmatch/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.status === 400) {
+          console.log("error");
+        }
+        if (res === true) {
+         return setMatch(true)
+        }
+        if (res === false) console.log("not a match");
+        return advanceQ();
+      });
   }
 
   function advanceQ() {
@@ -96,70 +84,58 @@ export function DiscoverLayout({ userQueue, qCounter }) {
     setMatch(false);
   }
 
-  /* 
-    function handleSwipe () { 
-        advanceQ()
-    }
-     */
+
 
   return (
     <>
-    {qCounter.current === userQueue.length ? (
-      <Box maxWidth="100vw" minHeight={"80vh"} flexBasis="auto">
-        <Paper elevation={8} sx={{ borderRadius: "8rem", opacity: ".6" }}>
+      {qCounter.current === userQueue.length ? (
+        <Box maxWidth="100vw" minHeight={"80vh"} flexBasis="auto">
+          <Paper elevation={8} sx={{ borderRadius: "8rem", opacity: ".6" }}>
+            <Typography
+              variant="h6"
+              sx={{ textAlign: "center", marginTop: "30vh", padding: "1rem" }}
+            >
+              There is no one new to display.
+            </Typography>
+          </Paper>
+        </Box>
+      ) : (
+        <>
           <Typography
-            variant="h6"
-            sx={{ textAlign: "center", marginTop: "30vh", padding: "1rem" }}
+            variant="h3"
+            elevation={24}
+            marginTop="2rem"
+            sx={{ color: "black" }}
           >
-            There is no one new to display.
+            {userQueue[qCounter.current].owner.username}
           </Typography>
-        </Paper>
-      </Box>
-    ) : (
-    <>
-    
-      <Typography
-        variant="h3"
-        ref={titleRef}
-        elevation={24}
-        marginTop="2rem"
-        sx={{ color: "black" }}
-      >
-        {userQueue[qCounter.current].owner.username}
-      </Typography>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <IconButton
-          onClick={() => {
-            handleSwipe("Left");
-          }}
-        >
-          <ThumbDownOffAltRounded sx={{ fontSize: "2.5rem" }} />
-        </IconButton>
-        <DiscoverPaper
-          currentPosition={qCounter.current}
-          userQueue={userQueue}
-          titleRef={titleRef}
-        />
-        <IconButton
-          onClick={() => {
-            handleSwipe("Right");
-          }}
-        >
-          <ThumbUp sx={{ fontSize: "2.5rem" }} />
-        </IconButton>
-        <MatchActionButtons
-          currentUser={qCounter.current}
-          userQueue={userQueue}
-        />
-      </Box>
-     
-      {/*  <Stack flexDirection="row" marginTop="2rem" columnGap="3rem">
-        <NextAvatar />
-        <NextAvatar />
-        <NextAvatar />
-      </Stack> */}
-    </>
-    )}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton
+              onClick={() => {
+                handleSwipe("Left");
+              }}
+            >
+              <ThumbDownOffAltRounded sx={{ fontSize: "2.5rem" }} />
+            </IconButton>
+            <DiscoverPaper
+              currentPosition={qCounter.current}
+              userQueue={userQueue}
+            />
+            <IconButton
+              onClick={() => {
+                handleSwipe("Right");
+              }}
+            >
+              <ThumbUp sx={{ fontSize: "2.5rem" }} />
+            </IconButton>
+            <MatchActionButtons
+              currentUser={qCounter.current}
+              userQueue={userQueue}
+            />
+          </Box>
+              <MatchSnackbar match={match} setMatch={setMatch} advanceQ={advanceQ}/>
+        </>
+      )}
     </>
   );
 }
--- src/components/feedbackAndNotifs/MatchSnackbar.js
+++ src/components/feedbackAndNotifs/MatchSnackbar.js
@@ -1,35 +1,44 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { Typography, IconButton, Icon } from "@mui/material";
+import NextPlanIcon from '@mui/icons-material/NextPlan';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import { Chat } from "@mui/icons-material";
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function MatchSnackbar({match}) {
-  const [open, setOpen] = React.useState(match);
-
-  React.useEffect(()=> {
-    console.log(open + "is currently open")
-  })
-
-
+export default function MatchSnackbar({ match, setMatch, advanceQ }) {
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
-    setOpen(false);
+    setMatch(false);
+    advanceQ()
   };
 
   return (
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          This is a success message!
-        </Alert>
-      </Snackbar>
-
+    <Snackbar
+      open={match}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      display="flex"
+      sx={{alignItems: "center"}}
+    >
+      <Alert onClose={handleClose}  sx={{ width: "100vw", backgroundColor: "black"}}>
+        <Stack direction="row"  spacing={{ xs: 1, sm: 2, md: 4 }}> 
+        <Typography /* sx={{paddingRight: "5vw"}} */ variant="h5">Match!</Typography>
+      
+        {/* <IconButton ><ChatBubbleOutlineIcon></ChatBubbleOutlineIcon></IconButton>
+        <IconButton><NextPlanIcon></NextPlanIcon></IconButton> */}
+        <Button variant="contained" startIcon={<ChatBubbleOutlineIcon/>}>Message</Button>
+        <Button variant="contained" startIcon={<NextPlanIcon/>}>Next</Button>
+        </Stack>
+      </Alert>
+    </Snackbar>
   );
 }
