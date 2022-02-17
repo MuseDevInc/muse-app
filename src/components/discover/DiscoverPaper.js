1c4bdd3fb7655a46985faa6d5eed951cfa943eab
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-17 02:08:33 -0700

    Cleaned up  DiscoverPaper

--- src/components/discover/DiscoverPaper.js
+++ src/components/discover/DiscoverPaper.js
@@ -1,16 +1,13 @@
-import {Paper, Card, Typography, Container} from "@mui/material"
-import { AlbumContainer } from "./AlbumContainer";
-import { MatchedUserTabs } from "./MatchedUserTabs";
+import {Paper} from "@mui/material"
+import { AlbumContainer } from "./AlbumContainer";;
 
-export const DiscoverPaper = ({currentPosition, userQueue}) => {
+export function DiscoverPaper ({currentPosition, userQueue}) {
 
   return (
-    <Card elevation={16} sx={{/* borderRadius: 5, */ marginTop:"1.5rem", marginX: "1rem", maxWidth: "85%", flexBasis: "auto", }} >
-   
-  {/*       <MatchedUserTabs sx={{flex: "5"}}/> */}
+    <>
+     <Paper elevation={16} sx={{marginTop:"1.5rem", marginX: "1rem", maxWidth: "85%", flexBasis: "auto" }} > 
         <AlbumContainer currentPosition={currentPosition} userQueue={userQueue}/>
-{/*         <Typography variant="h3">Christian Staubo</Typography> */}
-    
-    </Card>
+    </Paper>
+   </>
   );
 };
