bcc383e100cf99eaf4f4273b9599cd1977dce8b9
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-17 02:21:21 -0700

    cleaned up Main, deleted ClipWrapper, DiscoverContainer, FAB component MatchActionButtons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,10 @@
-import { Typography, Paper, Stack } from "@mui/material";
-import React, {useEffect, useContext} from "react";
+import React from "react";
 import { NavBar } from "./navbar/NavBar";
-import { Login } from "./formComponents/Login";
-import { RegisterSession } from "./formComponents/RegisterSession";
-import { DiscoverPaper } from "./discover/DiscoverPaper";
-import { DiscoverContainer } from "./discover/DiscoverContainer";
-import { PlaybackControls } from "./discover/PlaybackControls";
 import { MuseWindow } from "./MuseWindow";
 
 export function Main() {
-
-
   let backGrad = "linear-gradient(1deg, #00377C 40%, #F5F5F5)";
+
   return (
     <div>
       <NavBar />
--- src/components/discover/DiscoverContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function DiscoverContainer() {
-  return <div></div>;
-}
-
-
