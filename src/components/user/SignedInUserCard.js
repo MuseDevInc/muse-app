e04dae3aabb50d70438e5d58460bad1b24f61393
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-17 21:11:06 -0700

    Autocomplete bug fix and styled genres field on signedinusercard

--- src/components/user/SignedInUserCard.js
+++ src/components/user/SignedInUserCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Typography,
   Paper,
@@ -13,6 +13,7 @@ import {
   Divider,
   Stack,
   styled,
+  Chip,
 } from "@mui/material";
 import { red } from "@mui/material/colors";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -29,116 +30,118 @@ const Root = styled("div")(({ theme }) => ({
   },
 }));
 
-const SignedInUserCard = ({username, displayProfile, handleNavToEdit}) => {
+const SignedInUserCard = ({ username, displayProfile, handleNavToEdit }) => {
   const [currentPlayback, setCurrentPlayback] = useState();
-
-  useEffect(() => {
-    console.log(username);
-    // console.log(displayProfile);
-  }, []);
 
   return (
     <>
-        <Paper
-          elevation={8}
+      <Paper
+        elevation={8}
+        sx={{
+          minHeight: "100vh",
+          maxHeight: "100vh",
+          background: `${backGrad}`,
+        }}
+      >
+        <Stack
           sx={{
-            minHeight: "100vh",
-            maxHeight: "100vh",
-            background: `${backGrad}`,
+            alignItems: "center",
+            overflow: "scroll",
+            paddingBottom: "1rem",
           }}
         >
-          <Stack
+          <Card
             sx={{
-              alignItems: "center",
-              overflow: "scroll",
-              paddingBottom: "1rem",
+              maxWidth: 500,
+              padding: "2rem",
+              margin: "2rem",
+              position: "absolute",
             }}
           >
-            <Card
-              sx={{
-                maxWidth: 500,
-                padding: "2rem",
-                margin: "2rem",
-                position: "absolute",
-              }}
-            >
-              <CardHeader
-                avatar={
-                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                    {username[0].toUpperCase()}
-                  </Avatar>
-                }
-                action={
-                  <IconButton
-                    onClick={handleNavToEdit}
-                    aria-label="upload picture"
-                    component="span"
-                  >
-                    <EditIcon />
-                  </IconButton>
-                }
-                title={username}
-                subheader="New York City, New York"
-              />
-              <CardMedia
-                component="img"
-                height="360"
-                image={
-                  displayProfile.favSongs.length > 0
-                    ? displayProfile.favSongs[0].albumUrl
-                    : null
-                }
-                alt={
-                  displayProfile.favSongs.length > 0
-                    ? displayProfile.favSongs[0].title
-                    : null
-                }
-              />
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {username[0].toUpperCase()}
+                </Avatar>
+              }
+              action={
+                <IconButton
+                  onClick={handleNavToEdit}
+                  aria-label="upload picture"
+                  component="span"
                 >
-                  <Typography>About Me</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>{displayProfile.aboutMe}</Typography>
-                </AccordionDetails>
-              </Accordion>
-
-              <Root>
-                <Divider sx={{ padding: "1rem", textAlign: "center" }}>
-                  Favorite Genre
-                </Divider>
-                {displayProfile.favGenres}
-                <Divider>Favorite Album of All Time</Divider>
-              </Root>
-              <Stack
-                direction="row"
-                divider={<Divider orientation="vertical" flexItem />}
-                spacing={1}
-                sx={{ margin: "1rem", alignItems: "space" }}
-              >
-                {displayProfile.favSongs.map((song) => {
-                  return <MusicPlayer key={song.uri} song={song} setCurrentPlayback={setCurrentPlayback}
-                  />;
-                })}
-              </Stack>
-              <Stack>
-              {currentPlayback && (
-            <iframe
-              title="Spotify"
-              src={`https://embed.spotify.com/?uri=${currentPlayback}&theme=black`}
-              height="90"
-              frameBorder="0"
-              allowtransparency="true"
+                  <EditIcon />
+                </IconButton>
+              }
+              title={username}
+              subheader="New York City, New York"
             />
-          )}
-              </Stack>
-            </Card>
-          </Stack>
-        </Paper>
+            <CardMedia
+              component="img"
+              height="360"
+              image={
+                displayProfile.favSongs.length > 0
+                  ? displayProfile.favSongs[0].albumUrl
+                  : null
+              }
+              alt={
+                displayProfile.favSongs.length > 0
+                  ? displayProfile.favSongs[0].title
+                  : null
+              }
+            />
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>About Me</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{displayProfile.aboutMe}</Typography>
+              </AccordionDetails>
+            </Accordion>
+
+            <Root>
+              <Divider sx={{ padding: "1rem", textAlign: "center" }}>
+                Favorite Genre
+              </Divider>
+              {displayProfile.favGenres.map((genre) => {
+                return <>{<Chip key={genre} label={genre} />}</>;
+              })}
+              <Divider>Favorite Album of All Time</Divider>
+            </Root>
+            <Stack
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={1}
+              sx={{ margin: "1rem", alignItems: "space" }}
+            >
+              {displayProfile.favSongs.map((song) => {
+                return (
+                  <MusicPlayer
+                    key={song.uri}
+                    song={song}
+                    setCurrentPlayback={setCurrentPlayback}
+                  />
+                );
+              })}
+            </Stack>
+            <Stack>
+              {currentPlayback && (
+                <iframe
+                  title="Spotify"
+                  src={`https://embed.spotify.com/?uri=${currentPlayback}&theme=black`}
+                  height="90"
+                  frameBorder="0"
+                  allowtransparency="true"
+                />
+              )}
+            </Stack>
+          </Card>
+        </Stack>
+      </Paper>
     </>
   );
 };
