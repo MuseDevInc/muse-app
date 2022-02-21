f5c82fa5593462283a8d709c132af1b5cd008aa4
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-21 00:39:23 -0700

    fixed insane bug re overflow x not working on mobile and also resized albumcontainer

--- src/components/discover/AlbumContainer.js
+++ src/components/discover/AlbumContainer.js
@@ -17,6 +17,7 @@ export const AlbumContainer = ({ currentPosition, userQueue }) => {
         alt="favorite album"
         image={userQueue[currentPosition].favSongs[0]?.albumUrl}
         draggable={false}
+        sx={{maxHeight: "60vh"}}
       />
       <IconButton onClick={handleShowMore}>
         <MoreVertIcon />
