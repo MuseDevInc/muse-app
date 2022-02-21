798633a32725d1fd978183a1e3bea9cb0050f9ae
jmwalsh91 <jordanmwalsh@gmail.com>
2022-02-20 22:29:55 -0700

    improved swipe trigger position on x axis

--- src/components/discover/DiscoverPaper.js
+++ src/components/discover/DiscoverPaper.js
@@ -7,7 +7,7 @@ import {
   motion,
   useAnimation,
   useMotionValue,
-  useIsPresent
+  useIsPresent,
 } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
@@ -16,44 +16,31 @@ export function DiscoverPaper({ handleSwipe, currentPosition, userQueue }) {
   const [startX, setStartX] = useState();
   const controls = useAnimation();
   const [target, setTarget] = useState();
-  const isPresent = useIsPresent()
-  const [isShown, setIsShown] = useState(true)
+  const isPresent = useIsPresent();
+  const [isShown, setIsShown] = useState(true);
   /* 
   useEffect(() => {
     console.log(startX)
   }) */
 
-
-  useEffect(()=>{
-    console.log(isPresent)
-  controls.start({
-    scale: 1,
-    transition: {delay: 2, duration: 2}
-  })
-  }, [currentPosition])
+/*   useEffect(() => {
+    console.log(isPresent);
+    controls.start({
+      scale: 1,
+      transition: { delay: 2, duration: 2 },
+    });
+  }, [currentPosition]); */
 
   function swipePoint(swipeX) {
     console.log("yeeeeeeeeet");
     let direction;
-    if (swipeX / startX > 2.5) {
+    if (swipeX / startX > 1.5) {
       direction = "Right";
-  /*     controls.start({
-        transition: {
-          when: "beforeChildren",
-          staggerChildren: 0.3,
-        },
-      }) */
     }
-    if (swipeX / startX < 0.4) {
+    if (swipeX / startX < 0.5) {
       direction = "Left";
-
     }
-
     return leaveScreen(direction, swipeX);
-  }
-
-  function travelDistance(endPoint) {
-    return endPoint * 2.2;
   }
 
   function leaveScreen(direction, startPoint) {
@@ -64,29 +51,26 @@ export function DiscoverPaper({ handleSwipe, currentPosition, userQueue }) {
   return (
     <>
       <AnimatePresence exitBeforeEnter>
-
         <motion.div
-     /*    animate={controls} */
-           variants={{
-             visible: { opacity: 1, scale: 1 },
-             hidden: { opacity: 0, scale: 0 }
-           }}
-
-          initial={{opacity: 0}}
-           animate={{opacity: 1}}
-          
-
           key={currentPosition}
-
-          transition={{delay: 1, duration: 1, tween: 3  }}
-          exit={{ opacity: 0, duration: 1, transition: {easeOut: [.9, .5, .3, .1] }}}
-
+          /*    animate={controls} */
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1, duration: 1, tween: 3 }}
+          exit={{
+            opacity: 0,
+            duration: 1,
+            transition: { easeOut: [0.9, 0.5, 0.3, 0.1] },
+          }}
           drag="x"
           dragConstraints={{
             left: 0,
-            right: 0
+            right: 0,
           }}
-
           onDragStart={(event, info) => {
             setStartX(info.point.x);
           }}
@@ -95,29 +79,26 @@ export function DiscoverPaper({ handleSwipe, currentPosition, userQueue }) {
           }}
           /*  onDrag={(event, info) => console.log(info.point.x, info.point.y, positionRef.current.parentNode.offsetWidth, )} */
 
-     
-          
-    
           whileHover={{
             scale: 0.99,
           }}
         >
           <Paper
-               elevation={16}
-               sx={{
-                zIndex: "8000",
-                 marginTop: "1.5rem",
-                 marginX: "1rem",
-                 maxWidth: "80vw",
-                 flexBasis: "auto"}}>
+            elevation={16}
+            sx={{
+              zIndex: "8000",
+              marginTop: "1.5rem",
+              marginX: "1rem",
+              maxWidth: "80vw",
+              flexBasis: "auto",
+            }}
+          >
             <AlbumContainer
-          
               currentPosition={currentPosition}
               userQueue={userQueue}
             />
-            </Paper>
-  </motion.div>
-
+          </Paper>
+        </motion.div>
       </AnimatePresence>
     </>
   );
